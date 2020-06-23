797189b3d8da9954da4a00fdbc766bf75d336bdf
Nilesh Solanki <nileshsolanki456@gmail.com>
2020-06-23 20:51:06 +0530

    make heroku compatible

--- api/app.js
+++ api/app.js
@@ -401,7 +401,7 @@ let deleteTasksFromList = (_listId) => {
 
 
 
-
-app.listen(3000, () => {
-    console.log("Server is listening on port 3000");
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log("Server is listening on port " + PORT);
 })
